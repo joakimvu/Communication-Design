2c5e16b72280cd243b02313c7f919f585d522802
joakimvu <joakim.vu@hiof.no>
2022-09-15 13:44:26 +0200

    Update index.js
    More smooth last text change color

--- article-template/index.js
+++ article-template/index.js
@@ -10,15 +10,16 @@ const onScroll = () => {
   const divHeight = firstDiv.clientHeight;
   const bodyHeight = document.body.clientHeight;
   const scrollHeight = document.documentElement.scrollTop;
+  const textHeight = firstText.clientHeight;
   const heightDiff = bodyHeight - divHeight;
-  console.log(scrollHeight, heightDiff, firstText.clientHeight);
+  console.log(scrollHeight, heightDiff, textHeight);
   // console.log(
   //   scrollHeight,
   //   heightDiff + firstText.clientHeight - divHeight / 2
   // );
   //   console.log(document.documentElement.scrollTop, height);
 
-  if (scrollHeight > heightDiff - 0.5 * divHeight) {
+  if (scrollHeight > heightDiff - textHeight / 2 - divHeight / 2) {
     firstText.className = "black-text";
     // secondText.className = "white-text";
   } else {
